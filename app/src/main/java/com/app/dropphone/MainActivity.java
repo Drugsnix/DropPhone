62d1291fb0ce2be8444210b123bad9d6f703f8ba
Anders la Cour <Anderslcp@hotmail.com>
2018-03-19 16:18:17 +0100

    Accellometer: Listener der kun gør sit + den stoppes når app pauses. samt startes igen når app startes fra pause

--- app/src/main/java/com/app/dropphone/MainActivity.java
+++ app/src/main/java/com/app/dropphone/MainActivity.java
@@ -12,11 +12,12 @@ import android.widget.TextView;
 import org.w3c.dom.Text;
 
 
-public class MainActivity extends AppCompatActivity implements  SensorEventListener{
+public class MainActivity extends AppCompatActivity{
 
     private TextView xText, yText, zText;
     private Sensor accelSensor;
     private SensorManager sManager;
+    private SensorEventListener accelListener;
 
 
     @Override
@@ -27,27 +28,65 @@ public class MainActivity extends AppCompatActivity implements  SensorEventListe
         //makes the Sensormanager
         sManager = (SensorManager)getSystemService(SENSOR_SERVICE);
         //Sets the accelerometer
+        //https://www.youtube.com/watch?v=YrI2pCZC8cc
         accelSensor = sManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
 
-        sManager.registerListener(this,accelSensor,SensorManager.SENSOR_DELAY_FASTEST);
+        //sManager.registerListener(this,accelSensor,SensorManager.SENSOR_DELAY_FASTEST);
 
 
-        xText = (TextView)findViewById(R.id.xText);
-        yText = (TextView)findViewById(R.id.yText);
-        zText = (TextView)findViewById(R.id.zText);
+        xText = findViewById(R.id.xText);
+        yText = findViewById(R.id.yText);
+        zText = findViewById(R.id.zText);
+
+        //accelListener får body som den tjekker konstant
+        accelListener = new SensorEventListener()
+        {
+            float xtop, ytop,ztop;
+            @Override
+            public void onSensorChanged(SensorEvent sensorEvent)
+            {
+                float xtmp = sensorEvent.values[0];
+                float ytmp = sensorEvent.values[1];
+                float ztmp = sensorEvent.values[2];
+                if(xtop<xtmp)
+                {
+                    xtop = xtmp;
+                    xText.setText("X: "+ xtop); //max so far= 19.483f
+                }
+                if(ytop<ytmp)
+                {
+                    ytop = ytmp;
+                    yText.setText("Y: "+ ytop); //max so far= 19.618f
+                }
+                if(ztop<ztmp)
+                {
+                    ztop = ztmp;
+                    zText.setText("Z: "+ ztop); //max so far= 19.245f
+                }
+            }
+
+            @Override
+            public void onAccuracyChanged(Sensor sensor, int i) {
+
+            }
+        };
     }
 
+
     @Override
-    public void onSensorChanged(SensorEvent sensorEvent)
+    protected void onResume()
     {
-    xText.setText("X: "+ sensorEvent.values[0]);
-    yText.setText("Y: "+ sensorEvent.values[1]);
-    zText.setText("Z: "+ sensorEvent.values[2]);
+        //når mobilen resumer appen skal listeneren sættes igen
+        super.onResume();
+        sManager.registerListener(accelListener,accelSensor,SensorManager.SENSOR_DELAY_FASTEST);
     }
 
     @Override
-    public void onAccuracyChanged(Sensor sensor, int i) {
-
+    protected void onPause()
+    {
+        //når mobilen pauser appen skal listeneren slukkes
+        super.onPause();
+        sManager.unregisterListener(accelListener);
     }
 
     private void TestThing()
